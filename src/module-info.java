7981899c2110a8a39e7a52ed5155ee50acefac19
lengoquocbo <quocbo303@gmail.com>
2024-05-12 20:38:39 +0700

    index

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Encode {
+	requires java.desktop;
+	requires java.sql;
+	requires jBCrypt;
+}
---
+++ src/view/homepage.java
@@ -0,0 +1,49 @@
+package view;
+
+
+
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class homepage extends JFrame {
+	public homepage() {
+		getContentPane().setBackground(new Color(255, 250, 205));
+		Container con = getContentPane();
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome!");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 23));
+		lblNewLabel.setBounds(228, 136, 172, 31);
+		getContentPane().add(lblNewLabel);
+		
+		
+		setSize(640, 360);
+        setTitle("Trang chủ");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					homepage frame = new homepage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
